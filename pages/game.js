2108e99e670fa0ccec417dc46be161b7d2a3d8de
Trezy <tre@trezy.com>
2019-04-26 21:59:28 -0500

    feat(/game): add a page for the game

---
+++ pages/game.js
@@ -0,0 +1,28 @@
+// Module imports
+import React from 'react'
+
+
+
+
+
+// Component imports
+import { PageWrapper } from '../components'
+
+
+
+
+const Game = () => (
+  <PageWrapper title="GAEM">
+    <header>
+      <h2>It's a heading!</h2>
+    </header>
+
+    <p>It's a text!</p>
+  </PageWrapper>
+)
+
+
+
+
+
+export default Game
