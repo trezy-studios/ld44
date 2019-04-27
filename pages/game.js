bbb0ea0b582d0936c88cbe80b914a8f2560a674f
Trezy <tre@trezy.com>
2019-04-26 23:47:51 -0500

    fix(/game): move Game into its own component

--- pages/game.js
+++ pages/game.js
@@ -12,56 +12,22 @@ import { isBrowser } from '../helpers'
 
 
 
-class LD44 extends React.Component {
-  componentDidMount () {
-    if (isBrowser()) {
-      /* eslint-disable global-require */
-      const Phaser = require('phaser')
-      /* eslint-enable */
+const Game = () => {
+  if (isBrowser()) {
+    /* eslint-disable global-require */
+    const { GameComponent } = require('../components/GameComponent')
+    /* eslint-enable */
 
-      const { Game, WEBGL } = Phaser
-
-      this.game = new Game({
-        canvas: this.canvas.current,
-        type: WEBGL,
-        physics: {
-          default: 'arcade',
-          arcade: {
-            gravity: { y: 200 },
-          },
-        },
-        scale: {
-          height: window.innerHeight,
-          mode: Phaser.Scale.RESIZE,
-          parent: 'body',
-          width: window.innerWidth,
-        },
-        scene: {
-          create: () => {},
-          preload: () => {},
-        },
-      })
-    }
-  }
-
-  constructor (props) {
-    super(props)
-    this.canvas = React.createRef()
-  }
-
-  render () {
     return (
-      <>
-        {/* eslint-disable jsx-a11y/control-has-associated-label */}
-        <canvas ref={this.canvas} />
-        {/* eslint-enable */}
-      </>
+      <GameComponent />
     )
   }
+
+  return null
 }
 
 
 
 
 
-export default LD44
+export default Game
