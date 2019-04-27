7350e74ee35e09784a4166b11c175569f3f362f1
Kirk M <kirk@saricden.com>
2019-04-26 21:12:17 -0700

    Phaser liiives!

--- pages/game.js
+++ pages/game.js
@@ -6,23 +6,26 @@ import React from 'react'
 
 
 // Local imports
-import { PageWrapper } from '../components'
 import { isBrowser } from '../helpers'
 
 
 
 
 
-class Game extends React.Component {
+class LD44 extends React.Component {
   componentDidMount () {
     if (isBrowser()) {
       /* eslint-disable global-require */
-      this.phaser = require('phaser')
+      const Phaser = require('phaser')
       /* eslint-enable */
 
-      this.game = new this.phaser.Game({
+      const { Game, WEBGL } = Phaser
+
+      this.game = new Game({
         canvas: this.canvas.current,
-        height: 600,
+        type: WEBGL,
+        height: window.innerHeight,
+        width: window.innerWidth,
         physics: {
           default: 'arcade',
           arcade: {
@@ -33,8 +36,6 @@ class Game extends React.Component {
           create: () => {},
           preload: () => {},
         },
-        type: this.phaser.AUTO,
-        width: 800,
       })
     }
   }
@@ -46,15 +47,11 @@ class Game extends React.Component {
 
   render () {
     return (
-      <PageWrapper title="GAEM">
-        <header>
-          <h2>It's a heading!</h2>
-        </header>
-
+      <div>
         {/* eslint-disable jsx-a11y/control-has-associated-label */}
         <canvas ref={this.canvas} />
         {/* eslint-enable */}
-      </PageWrapper>
+      </div>
     )
   }
 }
@@ -63,4 +60,4 @@ class Game extends React.Component {
 
 
 
-export default Game
+export default LD44
