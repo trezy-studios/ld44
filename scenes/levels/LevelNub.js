1834a8f3b5d192cb63c9d95293222a3e2900d544
IanSSenne <IanSSenne@gmail.com>
2019-04-28 16:54:26 +0000

    started dev level and added left-right controll using arrows and aswd controll schemes disabled no-bitwise rule in .eslintrc

--- scenes/levels/LevelNub.js
+++ scenes/levels/LevelNub.js
@@ -1,4 +1,4 @@
-import { Scene } from 'phaser'
+import { Scene, Input } from 'phaser'
 
 class LevelNub extends Scene {
   constructor () {
@@ -6,11 +6,26 @@ class LevelNub extends Scene {
   }
 
   create () {
-    console.log('start nub level', this)
+    this.state = new Map()
+    const playerX = 50
+    const playerY = 50
+    this.state.set('player', this.physics.add.sprite(playerX, playerY, 'hero'))
+    this.state.set('keys', this.input.keyboard.createCursorKeys())
+    console.log(this.state.get('keys'))
+    this.WSAD = {
+      w: this.input.keyboard.addKey(Input.Keyboard.KeyCodes.W),
+      s: this.input.keyboard.addKey(Input.Keyboard.KeyCodes.S),
+      a: this.input.keyboard.addKey(Input.Keyboard.KeyCodes.A),
+      d: this.input.keyboard.addKey(Input.Keyboard.KeyCodes.D),
+    }
   }
 
   update () {
-    console.log('ree', this)
+    const player = this.state.get('player')
+    const keyboard = this.state.get('keys')
+    const velocityX = (Number(keyboard.right.isDown) | Number(this.WSAD.d.isDown)) - (Number(keyboard.left.isDown) | Number(this.WSAD.a.isDown))
+    const magicNumber = 200
+    player.setVelocityX(velocityX * magicNumber)
   }
 }
 
